06e4aaf1be2481e615c33231325fa75094ed8b2e
Your Name <you@example.com>
2024-09-25 17:56:26 +0200

    Changed test name from Alek to Liam

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,7 +5,7 @@ function greet(name: string) {
 }
 
 test("should greet person", () => {
-  const result = greet("Alek");
+  const result = greet("Liam");
 
-  deepEqual(result, { message: "Welcome, Alek!" });
+  deepEqual(result, { message: "Welcome, Liam!" });
 });
